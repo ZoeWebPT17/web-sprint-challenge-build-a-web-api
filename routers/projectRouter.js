d2716b40cd09f9bab7957fb8110b68b384dfd7e9
Zoe Stokes <sekotszs@gmail.com>
2020-09-11 19:08:35 -0700

    project router done

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -10,26 +10,84 @@ router.get("/", (req, res) => {
     .catch((error) => {
       res
         .status(500)
-        .json({ error: "There was an error connecting to the database" });
+        .json({ error: "There was an error connecting to the database!" });
     });
 });
 
-router.get("/:id" (req,res) => {
+router.get("/:id", ValidateID, (req, res) => {
+  res.status(200).json(req.project);
+});
 
-})
+router.get("/:id/actions", ValidateID, (req, res) => {
+  database
+    .getProjectActions(req.params.id)
+    .then((result) => res.status(200).json(result))
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ error: "There was an error connecting to the database!" });
+    });
+});
 
-router.get("/:id/actions" (req,res) => {
-    
-})
+router.post("/", (req, res) => {
+  database
+    .insert(req.body)
+    .then((result) => res.status(201).send())
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ error: "There was an error connecting to the database!" });
+    });
+});
 
-router.post("/" (req,res) => {
-    
-})
+router.put("/:id", ValidateID, (req, res) => {
+  database
+    .update(req.params.id, req.body)
+    .then((result) => res.status(200).json(result))
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ error: "There was an error connecting to the database!" });
+    });
+});
 
-router.put("/:id" (req,res) => {
-    
-})
+router.delete("/:id", ValidateID, (req, res) => {
+  database
+    .remove(req.params.id)
+    .then((result) =>
+      result === 1
+        ? res.status(204).send()
+        : res
+            .status(500)
+            .json({ error: "There was an error connecting to the database!" })
+    )
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ error: "There was an error connecting to the database!" });
+    });
+});
 
-router.delete("/:id" (req,res) => {
-    
-})
+//Custom Middleware :D
+
+function ValidateID(req, res, next) {
+  database
+    .get(req.params.id)
+    .then((result) => {
+      if (result.length === 0) {
+        res
+          .status(404)
+          .json({ error: "This project ID is invalid! Try another!" });
+      } else {
+        req.project = result;
+        next();
+      }
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ error: "There was an error connecting to the database!" });
+    });
+}
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const server = express();
+const projectRouter = require("./routers/projectRouter");
 
 server.use(express.json());
 server.use(logger);
@@ -9,6 +10,8 @@ server.use(helmet());
 server.get("/", (req, res) => {
   res.send(`Web-Sprint-Challenge-Build-A-Web-API: Zoe Stokes`);
 });
+
+server.use("/api/projects", projectRouter);
 
 function logger(req, res, next) {
   console.log(req.method);
